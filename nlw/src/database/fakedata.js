94328ae5b0229a910ce8f973516f69d8fc774ddd
Yuri Fernandes da Silva <yuri.fernandes.961@gmail.com>
2020-11-14 01:21:37 -0300

    Commit Full Project

---
+++ nlw/src/database/fakedata.js
@@ -0,0 +1,35 @@
+module.exports = [
+    {
+        id: 1,
+        lat:"-23.5241752",
+        lng:"-46.1964738",
+        name: "Lar das meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            
+            "https://images.unsplash.com/photo-1576715159532-32eb4490f140?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+    
+            "https://images.unsplash.com/photo-1603138461420-e24168721842?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions:"Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours:"Horário de visitas Das 8h até 18h",
+        opening_on_weekends: "1"
+    },
+
+    {
+        id: 2,
+        lat:"-23.5241752",
+        lng:"-46.1994738",
+        name: "Lar dos meninos",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+          
+            "https://images.unsplash.com/photo-1576715159532-32eb4490f140?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+    
+            "https://images.unsplash.com/photo-1603138461420-e24168721842?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions:"Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours:"Horário de visitas Das 8h até 18h",
+        opening_on_weekends: "0"
+    }
+]
